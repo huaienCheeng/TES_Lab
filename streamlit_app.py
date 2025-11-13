600c93783fe8b6a92540b3eb583f3da4f083a3dc
huaienCheeng <huaiencheeng@gmail.com>
2025-11-13 15:09:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Demo Update Title")
 st.write(
-    " Hello! Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
