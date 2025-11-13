bd75113ff023837436857166536889d3089e781c
huaienCheeng <huaiencheeng@gmail.com>
2025-11-13 15:08:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Demo")
+st.title("🎈 Demo Update Title")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    " Hello! Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
